e4e35d927fb4264f6c66b282597b046c415025a9
Noah McCampbell <noahmccamp@aol.com>
2022-02-01 11:33:40 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import Subsonic
+
+struct ContentView: View {
+    let names = ["Bird", "Car", "Money"]
+    var body: some View {
+        VStack {
+            NavigationView {
+                ScrollView{
+                    ForEach(names, id: \.self){ name in
+                        Button{
+                            play(sound: "thunder.m4a")
+                        } label: {
+                            Image(name)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(25)
+                                .padding(.horizontal)
+                        }
+                    }
+                }
+                .navigationTitle("Noah's App")
+            }
+            .navigationViewStyle(.stack)
+        }
+    }
+}
