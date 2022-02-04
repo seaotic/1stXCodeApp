95c45a10022633c457f8b5e8c34e7d793c68d1b9
Noah McCampbell <noahmccamp@aol.com>
2022-02-04 13:33:49 -0500

    Test change for SwiftUI Basics

--- ContentView.swift
+++ ContentView.swift
@@ -3,27 +3,45 @@ import Subsonic
 
 struct ContentView: View {
     let names = ["Bird", "Car", "Money"]
+    @State var blockColor = Color.blue
     var body: some View {
-        VStack {
-            NavigationView {
-                ScrollView(.horizontal){
-                    HStack{
-                    ForEach(names, id: \.self){ name in
-                        Button{
-                            play(sound: "\(name).m4a")
-                        } label: {
-                            Image(name)
-                                .resizable()
-                                .scaledToFit()
-                                .cornerRadius(25)
-                                .padding(.horizontal)
+            ZStack {
+                //Zstack shows 3 ways to set up Views
+                Color.cyan
+                NavigationView {
+                    ScrollView(.horizontal){
+                        HStack{
+                            ForEach(names, id: \.self){ name in
+                                Button{
+                                    play(sound: "\(name).m4a")
+                                } label: {
+                                    Image(name)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .cornerRadius(25)
+                                        .padding(.horizontal)
+                                }
+                            }
+                            Text("Child Object")
                         }
                     }
-                    }
+                    .navigationTitle("Noah's App")
                 }
-                .navigationTitle("Noah's App")
+                .navigationViewStyle(.stack)
+                Text("Im ON TOP")
             }
-            .navigationViewStyle(.stack)
+        //Changed font on parent object and subsequently changed font on all child text objects.
+            .font(.title)
+        blockColor
+        Button("Change Color"){
+            if(blockColor == Color.blue){
+                self.blockColor = Color.green
+            } else if(blockColor == Color.green){
+                self.blockColor = Color.blue
+            }
         }
+        .foregroundColor(blockColor)
+        
+        
     }
 }
